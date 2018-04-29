9393503f496f0c8354dd7712d470912ed90d5194
Tyler Krings <w.tyler.krings@gmail.com>
2018-04-28 23:06:44 -0500

    view established

--- src/main/java/com/swole/swolemvc/models/RepCalculator.java
+++ src/main/java/com/swole/swolemvc/models/RepCalculator.java
@@ -10,33 +10,38 @@ import javax.persistence.OneToMany;
 
 
 abstract public class RepCalculator extends ORM {
-//lift method that i can call and onerepmax is an input
-    static public float[][] Lift(ORM oneRepMax) {
+    static public class WeekSchedule {
+        public String weekName;
+        public float[] reps;
+    }
 
-        float[][] todaysRep = new float[6][5];
+    private static final String[] numberNames = {
+            "Zero", "One:Work up to a 10RM then perform 70% of (TM) x 6 reps x 3+ sets", "Two:Work up to an 8RM then perform 75% of  (TM) x 5 reps x 3+ sets", "Three: Work up to a 6RM then perform 80% of (TM) x 4 reps x 3+ sets", "Four:Work up to a 4RM then perform 82.5% of (TM) x 3 reps x 3+ sets", "Five: Work up to an 2RM then perform 85% of (TM) x 2 reps x 4+ sets", "Six: Work up to a new One Rep Max then 100%(TM)"
+    };
 
+    private static final float[] weeklyMultipliers = {
+            0.7f, 0.75f, 0.8f, 0.825f, 0.85f, 1.f
+    };
 
-        float[] lift = new float[5];
-        lift[0] = oneRepMax.getDeadlift();
-        lift[1] = oneRepMax.getSquat();
-        lift[2] = oneRepMax.getOverheadpress();
-        lift[3] = oneRepMax.getBenchpress();
-        lift[4] = oneRepMax.getBentoverrow();
+    static public WeekSchedule[] Lift(ORM oneRepMax) {
+        WeekSchedule[] todaysRep = new WeekSchedule[6];
 
+        for (int i = 0; i<todaysRep.length; i++) {
+            WeekSchedule w = new WeekSchedule();
 
-        float[] week = new float[6];
-        week[0] = (7 / 10.f);
-        week[1] = (75 / 100.f);
-        week[2] = (8 / 10.f);
-        week[3] = (825 / 1000.f);
-        week[4] = (85 / 100.f);
-        week[5] = (1.f);
+            w.weekName = "Week " + numberNames[i+1];
 
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 5; j++) {
-                todaysRep[i][j] = week[i] * lift[j];
-            }
+            w.reps = new float[5];
+            float multiplier = weeklyMultipliers[i];
+            w.reps[0] = multiplier * oneRepMax.getDeadlift();
+            w.reps[1] = multiplier * oneRepMax.getSquat();
+            w.reps[2] = multiplier * oneRepMax.getOverheadpress();
+            w.reps[3] = multiplier * oneRepMax.getBenchpress();
+            w.reps[4] = multiplier * oneRepMax.getBentoverrow();
+
+            todaysRep[i] = w;
         }
+
         return todaysRep;
     }
 }
